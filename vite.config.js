f4e87424b68a4dd42c4d0b52a0617e6ca1d7fc27
Paul de Chassey <paul.debureteldechassey@wavestone.com>
2026-08-13 18:02:13 +0200

    chore: update API proxy to server IP 10.10.10.10:8000

--- vite.config.js
+++ vite.config.js
@@ -6,11 +6,11 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://team-server:8000',
+        target: 'http://10.10.10.10:8000',
         rewrite: path => path.replace(/^\/api/, '')
       },
       '/ws': {
-        target: 'ws://team-server:8000',
+        target: 'ws://10.10.10.10:8000',
         ws: true,
         changeOrigin: true
       }
